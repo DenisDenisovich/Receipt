1b81ddc2998c5069394a5a8e612f8c0fd90928f6
VladDenisov <luxsorr7@gmail.com>
2020-05-12 13:51:35 +0300

    RCA-32 Extract some code to method onMonthInteractor

--- app/src/main/java/shiverawe/github/com/receipt/domain/interactor/month/MonthInteractor.kt
+++ app/src/main/java/shiverawe/github/com/receipt/domain/interactor/month/MonthInteractor.kt
@@ -12,29 +12,41 @@ import java.util.concurrent.CancellationException
 
 class MonthInteractor(private val repository: IMonthRepository) : IMonthInteractor {
 
-    override suspend fun getMonthReceipt(dateFrom: Long, dateTo: Long): ReceiptResult<List<ReceiptHeader>> {
+    override suspend fun getMonthReceipt(
+        dateFrom: Long,
+        dateTo: Long
+    ): ReceiptResult<List<ReceiptHeader>> {
         if (!isOnline()) {
             return ReceiptResult(error = ReceiptError(type = ErrorType.OFFLINE))
         }
 
         return try {
+            // Get receipts from network and update db cache
             ReceiptResult(repository.getMonthReceipt(dateFrom, dateTo))
         } catch (e: CancellationException) {
+            // Coroutine is cancelled, return nothing
             ReceiptResult(isCancel = true)
         } catch (e: Exception) {
-            if (e is HttpException || !isOnline()) {
-                // Get db receipts if network error
-                try {
-                    val dbReceipts = repository.getMonthReceiptFromDb(dateFrom, dateTo)
-                    ReceiptResult(dbReceipts, ReceiptError(error = e, type = ErrorType.OFFLINE))
-                } catch (e: Exception) {
-                    // error while getting data from db. Return error without data
-                    ReceiptResult(error = ReceiptError(error = e, type = ErrorType.ERROR))
-                }
-            } else {
-                // return error without data
-                ReceiptResult(error = ReceiptError(error = e, type = ErrorType.ERROR))
-            }
+            onError(dateFrom, dateTo, e)
         }
     }
+
+    private suspend fun onError(
+        dateFrom: Long,
+        dateTo: Long,
+        e: Exception
+    ): ReceiptResult<List<ReceiptHeader>> =
+        if (e is HttpException || !isOnline()) {
+            // Get db receipts if network error
+            try {
+                val dbReceipts = repository.getMonthReceiptFromDb(dateFrom, dateTo)
+                ReceiptResult(dbReceipts, ReceiptError(error = e, type = ErrorType.OFFLINE))
+            } catch (e: Exception) {
+                // Error while getting data from db. Return error without data
+                ReceiptResult(error = ReceiptError(error = e, type = ErrorType.ERROR))
+            }
+        } else {
+            // Return error without data
+            ReceiptResult(error = ReceiptError(error = e, type = ErrorType.ERROR))
+        }
 }
