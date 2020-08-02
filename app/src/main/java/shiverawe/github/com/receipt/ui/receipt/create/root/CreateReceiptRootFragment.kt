32c708a3d097a8dbfac17e185419f27911d2ccdc
VladDenisov <luxsorr7@gmail.com>
2020-08-02 14:42:47 +0300

    RCA-55 change slide up animation

--- app/src/main/java/shiverawe/github/com/receipt/ui/receipt/create/root/CreateReceiptRootFragment.kt
+++ app/src/main/java/shiverawe/github/com/receipt/ui/receipt/create/root/CreateReceiptRootFragment.kt
@@ -70,7 +70,7 @@ class CreateReceiptRootFragment : BaseFragment(R.layout.fragment_create_receipt_
         childFragmentManager.beginTransaction().apply {
             if (currentScreen == CurrentScreen.QR) {
                 // ManualFragment isn't first screen. Open with animation
-                setCustomAnimations(R.anim.slide_up_2, R.anim.fade_out)
+                setCustomAnimations(R.anim.slide_up_alpha, R.anim.fade_out)
                 //setCustomAnimations(R.anim.slide_up, R.anim.fade_out, R.anim.fade_in, R.anim.slide_down)
             }
             addToBackStack(CurrentScreen.MANUAL.name)
